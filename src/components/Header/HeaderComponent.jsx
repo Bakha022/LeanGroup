cbeda1a9f116bb540cad490e401ff7692641ac13
Bakha022 <abduqahhorovbahodir536@gmail.com>
2024-06-19 12:02:24 +0500

    react slick add

--- src/components/Header/HeaderComponent.jsx
+++ src/components/Header/HeaderComponent.jsx
@@ -1,9 +1,46 @@
-import React, { Component } from 'react'
+import React from "react"
+import Slider from "react-slick"
 
-export default class HeaderComponent extends Component {
-	render() {
-		return (
-			<div>HeaderComponent</div>
-		)
+import './HeaderComponent.scss'
+
+export default function HeaderComponent() {
+	let settings = {
+		dots: true,
+		infinite: true,
+		speed: 500,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		nextArrow: false,
+		prevArrow: false,
 	}
+	return (
+		<section className='header'>
+			<Slider {...settings}>
+					<div className="header-items">
+						<div className="col-6">
+							<p>LEANGROUP - тубы и этикетки</p>
+							<h3>Ламинатные тубы</h3>
+							<p>Используются для производства зубных паст. Широко применяются в сегменте косметики, пищевой индустрии, парафармацевтике, бытовой химии и DIY (Do-it-Yourself).</p>
+							<button>Каталог</button>
+						</div>
+					</div>
+					<div className="header-items">
+						<div className="col-6">
+							<p>LEANGROUP - тубы и этикетки</p>
+							<h3>Ламинатные тубы</h3>
+							<p>Используются для производства зубных паст. Широко применяются в сегменте косметики, пищевой индустрии, парафармацевтике, бытовой химии и DIY (Do-it-Yourself).</p>
+							<button>Каталог</button>
+						</div>
+					</div>
+					<div className="header-items">
+						<div className="col-6">
+							<p>LEANGROUP - тубы и этикетки</p>
+							<h3>Ламинатные тубы</h3>
+							<p>Используются для производства зубных паст. Широко применяются в сегменте косметики, пищевой индустрии, парафармацевтике, бытовой химии и DIY (Do-it-Yourself).</p>
+							<button>Каталог</button>
+						</div>
+					</div>
+			</Slider>
+		</section>
+	)
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import "slick-carousel/slick/slick-theme.css"
+import "slick-carousel/slick/slick.css"
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <>
     <App />
